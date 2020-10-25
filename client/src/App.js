ad7757494f2c0d37d4164abe21166075aeb01e95
char502 <char502@hotmail.com>
2020-10-25 23:36:41 +0000

    added and mapped data from spaceX

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'
+import Launches from './components/Launches'
 import './App.css';
+import spaceXLogo from './images/spaceXLogo.png';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
 
 function App() {
   return (
-    <div className="App">
-      <h1>SpaceX</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img src={spaceXLogo} alt="SpaceX logo" style={{ width: 300, height: 150, display: 'block', margin: 'auto' }}/>
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
